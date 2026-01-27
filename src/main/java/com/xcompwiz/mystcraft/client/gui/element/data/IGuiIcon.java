74410c05a737640e1553b325440061f9210a07f2
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2026-01-27 12:59:58 -0700

    This made me very sad to do

--- src/main/java/com/xcompwiz/mystcraft/client/gui/element/data/IGuiIcon.java
+++
@@ -1,9 +0,0 @@
-package com.xcompwiz.mystcraft.client.gui.element.data;
-
-import net.minecraft.client.Minecraft;
-
-public interface IGuiIcon {
-
-	public void render(Minecraft mc, int guiLeft, int guiTop, int xSize, int ySize, float zLevel);
-
-}
